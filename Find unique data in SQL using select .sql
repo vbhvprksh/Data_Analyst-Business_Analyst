c6a50c78977dd61eb34b37887af6907d9402c2ca
Vaibhav Prakash <58112154+vbhvprksh@users.noreply.github.com>
2023-11-14 18:46:54 +0530

    Select and Select Distinct

---
+++ Find unique data in SQL using select .sql
@@ -0,0 +1,10 @@
+----- View All data from the table---
+Select * from [dbo].[db1]
+
+----- View Selective data from the table---
+
+Select Invoice,Quantity,Price from dbo.db1
+
+----- View Unique data from the table---
+
+Select DISTINCT(StockCode) from db1 
